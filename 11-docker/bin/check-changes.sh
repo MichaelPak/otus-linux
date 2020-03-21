d3659d87dcd06985fc5ca07261d15b86815aeff4
Michael Pak <michaelpak@Michaels-MacBook-Pro.local>
2020-03-21 11:03:14 +0300

    update 11-linux #1

--- 11-docker/bin/check-changes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-LATEST_COMMIT=$(git rev-parse HEAD)
-
-BACK_COMMIT=$(git log -1 --format=format:%H --full-diff 11-docker/back)
-
-if [[ ${DOCKER_COMMIT} = ${LATEST_COMMIT} ]]; then
-        echo "files in folder1 has changed"
-        .circleci/do_something.sh
---
+++ 11-docker/bin/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+BACK_COMMIT=$(git log -1 --format=format:%H --full-diff 11-docker/back)
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+build_and_push () {
+    cd 11-docker/back && pipenv lock --requirements > requirements.txt
+    docker build -t michaelpak/otus-linux-$1 .
+    docker push michaelpak/otus-linux-$1
+}
+
+if [[ ${BACK_COMMIT} = ${LATEST_COMMIT} ]]; then
+        echo "Files for backend has changed"
+        build_and_push back
+fi

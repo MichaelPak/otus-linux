3c3a639004779cff8cceedd8e6c1d763b65e266e
Michael Pak <michaelpak@Michaels-MacBook-Pro.local>
2020-03-21 11:54:23 +0300

    update 11-docker #4

--- 11-docker/README.md
+++ 11-docker/README.md
@@ -3,7 +3,8 @@
 
 |Backend|Frontend|
 |---|---|
-|   |   |
+|[![](https://images.microbadger.com/badges/image/michaelpak/otus-docker-back.svg)](https://microbadger.com/images/michaelpak/otus-docker-back "Get your own image badge on microbadger.com")||
+|[![](https://images.microbadger.com/badges/version/michaelpak/otus-docker-back.svg)](https://microbadger.com/images/michaelpak/otus-docker-back "Get your own version badge on microbadger.com")||
 
 
 
---
+++ 11-docker/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+BACK_COMMIT=$(git log -1 --format=format:%H --full-diff 11-docker/back)
+FRONT_COMMIT=$(git log -1 --format=format:%H --full-diff 11-docker/front)
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+build_and_push () {
+    cd 11-docker docker build --build-arg VCS_REF=`git rev-parse --short HEAD` -t michaelpak/otus-linux-$1 .
+    docker push michaelpak/otus-linux-$1
+}
+
+if [[ ${BACK_COMMIT} = ${LATEST_COMMIT} ]]; then
+    cd 11-docker/back && pipenv lock --requirements > requirements.txt
+    echo "Files for backend has changed"
+    build_and_push back
+elif [[ ${FRONT_COMMIT} = ${LATEST_COMMIT} ]]; then
+    echo "Files for frontend has changed"
+    build_and_push front
+fi
